e90f539b9f41a18e36d00b0365775236234373ca
sagar s.k <sks788056@gmail.com>
2026-01-10 18:44:25 +0545

    component setup for frontend

---
+++ frontend/src/Landing_page/About/AboutPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function AboutPage() {
+    return ( <>
+    </> );
+}
+
+export default AboutPage;
---
+++ frontend/src/Landing_page/Services/ServicesPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function ServicesPage() {
+    return ( <>
+    </> );
+}
+
+export default ServicesPage;
