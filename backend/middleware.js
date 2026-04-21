40b907a979656013cdc8dda67f052de27aa13e8a
sagar s.k <sks788056@gmail.com>
2026-04-22 02:30:36 +0545

    feat: add isLoggedIn JWT authentication middleware

---
+++ backend/middleware.js
@@ -0,0 +1,29 @@
+const { listingJoi , reviewJoi } = require("./schema") ;
+const Listing = require("./Models/ListingModel") ;
+const Review = require("./Models/ReviewModel") ;
+const ExpressError = require("./utils/ExpressError") ;
+const wrapAsync = require("./utils/wrapAsync") ;
+const jwt = require("jsonwebtoken") ;
+
+ module.exports.isLoggedIn = wrapAsync(async(req , res , next)=>{
+   const authHeader = req.headers.authorization ;
+   const token = authHeader && authHeader.split(" ")[1] ;  
+
+   if(!token){
+     throw new ExpressError(401 , "Access denied , Login first") ;
+   }
+
+   try{
+    const decodedPayLoad = jwt.verify(token , process.env.JWT_SECRET) ; 
+    req.user = decodedPayLoad ;
+    
+    next() ;
+   } catch(err){
+
+    throw new ExpressError(403 , "Token is invalid or expired") ;    
+   }
+ }) ;
+    
+
+
+
