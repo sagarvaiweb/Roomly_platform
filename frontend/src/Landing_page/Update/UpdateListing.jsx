bd77a5f1572b00cde9da32892af73ed4abf4f80f
sagar s.k <sks788056@gmail.com>
2026-05-02 14:44:58 +0545

    feat: integrate updateListingForm with backend API

--- frontend/src/Landing_page/Update/UpdateListing.jsx
+++ frontend/src/Landing_page/Update/UpdateListing.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import ListingForm from '../CommonComponents/ListingForm';
 import { allListings } from '../../Data/dummy';
+import axios from 'axios';
 
 function UpdateListing() {
 
      const { id } = useParams();
      const  navigate = useNavigate() ;
 
-     // Find the listing to edit
-     const listingToEdit = allListings.find((item) => item._id == id);
+     const [listingToEdit , setListingToEdit] = useState(null) ;
 
-     const handleUpdate = (formData) => {
-     
+     useEffect(()=>{
+        const fetchListing = async ()=>{
+            try{
+
+                const { data:{data : listing }} = await axios.get(`http://localhost:3000/listings/${id}`) ;
+                setListingToEdit(listing) ;
+                
+            }
+            catch(err){
+                console.error(err.response?.data?.message || err.message) ;
+            }
+        }
+
+        fetchListing() ;
+     } , [id])
+
+     const handleUpdate = async (formData) => {
+     const token = localStorage.getItem("token") ;
+
+     try{
+         const response = await axios.put(`http://localhost:3000/listings/${id}/update` , formData , { 
+            headers:{
+                Authorization: `Bearer ${token}`
+            }
+         }) ;
+
+         navigate(`/listings/${id}`);  // go to show page 
+         console.log(response.data.message) ;
+     }
+     catch(err){
+        console.error(err.response?.data?.message || err.message) ;
+     }
     
-      navigate(`/listings/${id}`);  // go to show page
   };
     return ( <>
      <ListingForm  initialData={listingToEdit} onSubmit={handleUpdate} description="Update Your listing"  buttonText="Update"  />
